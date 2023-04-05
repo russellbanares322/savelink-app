77c535d566019a1d3a8d683d024c54b828f03e29
russellbanares322 <banaresrussell11@gmail.com>
2023-04-05 11:02:05 +0800

    style: add padding in home component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./components/home/Home";
 
 function App() {
   return (
-    <div className="font-jetbrains flex flex-col justify-center items-center h-screen px-5 md:px-10 bg-light-blue min-h-[400px]">
+    <div className="font-jetbrains flex flex-col justify-center items-center h-full py-4 px-5 md:px-10 bg-light-blue min-h-[400px]">
       <div className="h-full w-full md:w-[45rem] flex-grow-1">
         <Home />
       </div>
