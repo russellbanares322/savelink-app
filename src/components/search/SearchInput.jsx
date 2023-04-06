463eef4fb1fc77471b91395cdf9369ce97208c17
russellbanares322 <banaresrussell11@gmail.com>
2023-04-06 20:18:58 +0800

    chore: add search icon

--- src/components/search/SearchInput.jsx
+++ src/components/search/SearchInput.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { HiOutlineSearch } from "react-icons/hi";
 
 const SearchInput = ({ setSearchInput }) => {
   return (
-    <div>
+    <div className="relative focus-within:text-white">
       <input
         onChange={(event) => setSearchInput(event.target.value)}
         className="my-3 w-full rounded-xl h-10 focus:ring-2 ring-blue bg-gray-600 p-4 font-bold text-white outline-none placeholder:font-normal"
         type="text"
         placeholder="Search links here..."
+      />
+      <HiOutlineSearch
+        className="absolute right-5 top-[23px] pointer-events-none"
+        size={18}
       />
     </div>
   );
--- src/hooks/useSearchInput.jsx
+++ src/hooks/useSearchInput.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const useSearchInput = (searchInput, data) => {
   if (!searchInput) {
     return data;
