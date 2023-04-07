683b82dc71b6ce047386b0f930b1462b80746a9f
russellbanares322 <banaresrussell11@gmail.com>
2023-04-07 12:00:35 +0800

    feat: add signout functionality

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
+import { signOut } from "firebase/auth";
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
 import {
   HiOutlineChevronDown,
   HiOutlineLink,
@@ -8,11 +10,13 @@ import {
 import { auth } from "../../config/firebaseConfig";
 import Form from "../form/Form";
 import logo from "/linksve_logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isOptionsOpen, setIsOptionsOpen] = useState(false);
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
   const handleToggleModal = (e) => {
     e.stopPropagation();
@@ -21,6 +25,16 @@ const Navbar = () => {
 
   const handleToggleOption = () => {
     setIsOptionsOpen(!isOptionsOpen);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      toast.success("Successfully logged out");
+      navigate("/sign-in");
+    } catch (err) {
+      toast.error(err.message);
+    }
   };
 
   return (
@@ -39,7 +53,7 @@ const Navbar = () => {
             />
           </p>
           <div
-            className={`bg-white h-auto w-auto absolute top-7 left-1 rounded-lg text-sm p-2 flex flex-col justify-start items-start duration-100 ease-in-out ${
+            className={`bg-white h-auto w-auto absolute top-7 left-24 rounded-lg text-sm p-2 flex flex-col justify-start items-start duration-100 ease-in-out ${
               !isOptionsOpen && "scale-0"
             }`}
           >
@@ -49,7 +63,10 @@ const Navbar = () => {
             >
               Add Link <HiOutlineLink />
             </p>
-            <p className="cursor-pointer hover:bg-blue hover:text-white p-1 rounded-lg flex justify-start items-center gap-1 w-full">
+            <p
+              onClick={handleLogout}
+              className="cursor-pointer hover:bg-blue hover:text-white p-1 rounded-lg flex justify-start items-center gap-1 w-full"
+            >
               Logout <HiOutlineLogout />
             </p>
           </div>
