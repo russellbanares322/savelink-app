f9d9766cb6b4b651112761930b038e4947270fa5
russellbanares322 <banaresrussell11@gmail.com>
2023-04-07 12:09:14 +0800

    feat: add logo and welcome text in signin page

--- src/pages/signin/GoogleSignin.jsx
+++ src/pages/signin/GoogleSignin.jsx
@@ -4,6 +4,7 @@ import toast from "react-hot-toast";
 import { FcGoogle } from "react-icons/fc";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../config/firebaseConfig";
+import logo from "/linksve_logo.png";
 
 const GoogleSignin = () => {
   const navigate = useNavigate();
@@ -21,13 +22,19 @@ const GoogleSignin = () => {
 
   return (
     <div className="flex items-center justify-center h-full">
-      <button
-        onClick={handleSignIn}
-        className="flex items-center justify-center gap-4 bg-blue text-white h-16 w-64 rounded-lg hover:bg-slate-900"
-      >
-        <FcGoogle className="pointer-events-none" size={25} /> Sign in with
-        Google
-      </button>
+      <div className="flex flex-col justify-center items-center bg-white py-10 px-5 gap-8 rounded-lg shadow-xl">
+        <img className="h-14 w-16" src={logo} />
+        <h1 className="font-bold text-blue">
+          Welcome to Linksve, save your important links.
+        </h1>
+        <button
+          onClick={handleSignIn}
+          className="flex items-center justify-center gap-4 bg-blue text-white h-12 w-60 md:h-16 md:w-64 rounded-lg hover:bg-slate-900"
+        >
+          <FcGoogle className="pointer-events-none" size={25} /> Sign in with
+          Google
+        </button>
+      </div>
     </div>
   );
 };
