5ecf04747f79d22c96fa92a3d4efb7b0081ae198
russellbanares322 <banaresrussell11@gmail.com>
2023-04-07 11:36:57 +0800

    feat: add dropdown menu in navbar instead of just button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,56 @@
 import React, { useState } from "react";
+import {
+  HiOutlineChevronDown,
+  HiOutlineLink,
+  HiOutlineLogout,
+} from "react-icons/hi";
 import Form from "../form/Form";
 import logo from "/linksve_logo.png";
 
 const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isOptionsOpen, setIsOptionsOpen] = useState(false);
 
   const handleToggleModal = (e) => {
     e.stopPropagation();
     setIsModalOpen(!isModalOpen);
   };
 
+  const handleToggleOption = () => {
+    setIsOptionsOpen(!isOptionsOpen);
+  };
+
   return (
     <div className="relative">
       <div className="flex justify-between items-center mt-5">
         <img className="h-12 w-14" src={logo} />
-        <button
-          onClick={handleToggleModal}
-          className="bg-blue text-white h-8 w-28 text-sm md:text-md md:h-8 md:w-36 rounded-lg"
-        >
-          Add link
-        </button>
+        <div className="relative">
+          <p className="text-sm flex justify-center items-center gap-2">
+            Hello, <strong>Name</strong>
+            <HiOutlineChevronDown
+              onClick={handleToggleOption}
+              className={`cursor-pointer duration-100 ease-out ${
+                isOptionsOpen && "rotate-[180deg]"
+              }`}
+              size={18}
+            />
+          </p>
+          <div
+            className={`bg-white h-auto w-auto absolute top-7 left-1 rounded-lg text-sm p-2 flex flex-col justify-start items-start duration-100 ease-in-out ${
+              !isOptionsOpen && "scale-0"
+            }`}
+          >
+            <p
+              onClick={handleToggleModal}
+              className="cursor-pointer hover:bg-blue hover:text-white p-1 rounded-lg flex justify-start items-center gap-1 w-full"
+            >
+              Add Link <HiOutlineLink />
+            </p>
+            <p className="cursor-pointer hover:bg-blue hover:text-white p-1 rounded-lg flex justify-start items-center gap-1 w-full">
+              Logout <HiOutlineLogout />
+            </p>
+          </div>
+        </div>
       </div>
       <Form isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </div>
