bcc26710a08aa48f46ff336014a627b5e4d8b662
russellbanares322 <banaresrussell11@gmail.com>
2023-04-10 22:28:10 +0800

    chore: rename variable loadedAll in pagination component

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { HiOutlineArrowSmDown, HiOutlineArrowSmUp } from "react-icons/hi";
 
 const Pagination = ({ paginate, data, filteredData, setPaginate }) => {
-  const loadedAll = filteredData.length === data.length;
+  const isLoadedAll = filteredData.length === data.length;
   const showButton = data.length > 4;
 
   const handleLoadContent = () => {
-    if (loadedAll && paginate !== 4) {
+    if (isLoadedAll && paginate !== 4) {
       setPaginate((prev) => prev - 4);
     } else {
       setPaginate((prev) => prev + 4);
@@ -23,12 +23,12 @@ const Pagination = ({ paginate, data, filteredData, setPaginate }) => {
         className="bg-blue text-white h-8 w-28 text-sm rounded-lg flex justify-center items-center"
         onClick={handleLoadContent}
       >
-        {loadedAll && (
+        {isLoadedAll && (
           <>
             Show Less <HiOutlineArrowSmUp size={20} />
           </>
         )}
-        {!loadedAll && (
+        {!isLoadedAll && (
           <>
             Show More <HiOutlineArrowSmDown size={20} />
           </>
