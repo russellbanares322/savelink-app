1121713965af26c534cd35e1dcc6eb92994bdcf0
russellbanares322 <banaresrussell11@gmail.com>
2023-12-09 21:17:54 +0800

    feat: implement sign in functionality

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,6 +1,9 @@
-import React from "react";
-import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/button/Button";
+import { auth } from "../../config/firebaseConfig";
 import { inputsData } from "./inputsData";
 
 const SignIn = () => {
@@ -8,14 +11,70 @@ const SignIn = () => {
     email: "",
     password: "",
   });
+  const [formSettings, setFormSettings] = useState({
+    isInputDirty: false,
+    isLoading: false,
+  });
+  const navigate = useNavigate();
+
+  const isInputFieldEmpty = (input) => {
+    if (input.trim().length === 0) {
+      return true;
+    }
+
+    return false;
+  };
 
   const handleInputChange = (e) => {
     const { value, id } = e.target;
     setFormData({ ...formData, [id]: value });
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setFormSettings({
+      ...formSettings,
+      isLoading: true,
+    });
+    if (
+      isInputFieldEmpty(formData.email) ||
+      isInputFieldEmpty(formData.password)
+    ) {
+      setFormSettings({
+        ...formSettings,
+        isInputDirty: true,
+      });
+    } else {
+      try {
+        const isUserSuccessfullySignedIn = await signInWithEmailAndPassword(
+          auth,
+          formData.email,
+          formData.password
+        );
+
+        if (isUserSuccessfullySignedIn) {
+          navigate("/");
+          setFormData({
+            email: "",
+            password: "",
+          });
+          setFormSettings({
+            isInputDirty: false,
+            isLoading: false,
+          });
+        }
+      } catch (err) {
+        toast.error(error.message);
+        setFormSettings({
+          ...formSettings,
+          isLoading: false,
+        });
+      }
+    }
+  };
+
   return (
-    <form className="mb-1">
+    <form onSubmit={onSubmit} className="mb-1">
       {inputsData.map((input) => {
         const inputId = input.id;
         return (
@@ -33,6 +92,10 @@ const SignIn = () => {
               type={input.type}
               placeholder={input.placeholder}
             />
+            {formSettings.isInputDirty &&
+              isInputFieldEmpty(formData[inputId]) && (
+                <p className="text-xs text-red">{input.label} is required</p>
+              )}
           </div>
         );
       })}
